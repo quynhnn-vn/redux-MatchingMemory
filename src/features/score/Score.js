91f8f49f036af31926de9d3dfaf4b576ee71bfe3
quynhnn-vn <nnquynh.0111@gmail.com>
2021-07-05 12:37:48 +0200

    Update styles

--- src/features/score/Score.js
+++ src/features/score/Score.js
@@ -10,8 +10,8 @@ export const Score = () => {
   // Display number of matched card pairs
   return (
     <div className="score-container">
-      Matched Pairs: {Math.floor(cardsMatched.length / 2)}
-      {currentBoard.length === cardsMatched.length && <p>You win !</p>}
+      <h2>Matched Pairs: {Math.floor(cardsMatched.length / 2)}</h2>
+      {currentBoard.length === cardsMatched.length && <p>You win!</p>}
     </div>
   );
 };
