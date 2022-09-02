56426a6f6eb9cfd7c0524b8c5190699dbea96261
quynhnn-vn <83249102+quynhnn-vn@users.noreply.github.com>
2022-09-02 20:24:42 +0200

    Update Card.js

--- src/features/board/cardRow/card/Card.js
+++ src/features/board/cardRow/card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectVisibleIds, flipCard, selectMatchedIds, resetCards } from "../../boardSlice";
-import cardLogo from ".card.jpeg"
+import cardLogo from "./card.jpeg"
 
 // let cardLogo =
 //  "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
